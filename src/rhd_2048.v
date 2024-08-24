6c431b7f8ff2d3d778f40c318274525ee6a29614
Hao Le <anhhao135@gmail.com>
2024-08-24 00:02:38 -0700

    added all rhd slaves for rhd 2048 tb

--- src/rhd_2048.v
+++ src/rhd_2048.v
@@ -42,7 +42,7 @@ module rhd_2048 (
     input wire [7:0] oversample_offset_P1,
     input wire [7:0] oversample_offset_P2,
 
-    output reg[1023:0] data_out,
+    output reg[32767:0] data_out,
 
     output wire CS,
     output wire SCLK,
@@ -145,6 +145,81 @@ module rhd_2048 (
     wire [15:0] data_out_a_A2;
     wire [15:0] data_out_b_A2;
 
+    wire [15:0] data_out_a_B1;
+    wire [15:0] data_out_b_B1;
+    wire [15:0] data_out_a_B2;
+    wire [15:0] data_out_b_B2;
+
+    wire [15:0] data_out_a_C1;
+    wire [15:0] data_out_b_C1;
+    wire [15:0] data_out_a_C2;
+    wire [15:0] data_out_b_C2;
+
+    wire [15:0] data_out_a_D1;
+    wire [15:0] data_out_b_D1;
+    wire [15:0] data_out_a_D2;
+    wire [15:0] data_out_b_D2;
+
+    wire [15:0] data_out_a_E1;
+    wire [15:0] data_out_b_E1;
+    wire [15:0] data_out_a_E2;
+    wire [15:0] data_out_b_E2;
+
+    wire [15:0] data_out_a_F1;
+    wire [15:0] data_out_b_F1;
+    wire [15:0] data_out_a_F2;
+    wire [15:0] data_out_b_F2;
+
+    wire [15:0] data_out_a_G1;
+    wire [15:0] data_out_b_G1;
+    wire [15:0] data_out_a_G2;
+    wire [15:0] data_out_b_G2;
+
+    wire [15:0] data_out_a_H1;
+    wire [15:0] data_out_b_H1;
+    wire [15:0] data_out_a_H2;
+    wire [15:0] data_out_b_H2;
+
+    wire [15:0] data_out_a_I1;
+    wire [15:0] data_out_b_I1;
+    wire [15:0] data_out_a_I2;
+    wire [15:0] data_out_b_I2;
+
+    wire [15:0] data_out_a_J1;
+    wire [15:0] data_out_b_J1;
+    wire [15:0] data_out_a_J2;
+    wire [15:0] data_out_b_J2;
+
+    wire [15:0] data_out_a_K1;
+    wire [15:0] data_out_b_K1;
+    wire [15:0] data_out_a_K2;
+    wire [15:0] data_out_b_K2;
+
+    wire [15:0] data_out_a_L1;
+    wire [15:0] data_out_b_L1;
+    wire [15:0] data_out_a_L2;
+    wire [15:0] data_out_b_L2;
+
+    wire [15:0] data_out_a_M1;
+    wire [15:0] data_out_b_M1;
+    wire [15:0] data_out_a_M2;
+    wire [15:0] data_out_b_M2;
+
+    wire [15:0] data_out_a_N1;
+    wire [15:0] data_out_b_N1;
+    wire [15:0] data_out_a_N2;
+    wire [15:0] data_out_b_N2;
+
+    wire [15:0] data_out_a_O1;
+    wire [15:0] data_out_b_O1;
+    wire [15:0] data_out_a_O2;
+    wire [15:0] data_out_b_O2;
+
+    wire [15:0] data_out_a_P1;
+    wire [15:0] data_out_b_P1;
+    wire [15:0] data_out_a_P2;
+    wire [15:0] data_out_b_P2;
+
     assign done = (state == REC_DONE);
     assign busy = (state != READY);
 
@@ -179,6 +254,230 @@ module rhd_2048 (
         .oversample_offset(oversample_offset_A2),
         .busy(busy_all[1]),
         .done(done_all[1])
+    );
+
+    rhd_spi_master B1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_B),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_B1),
+        .b_data_out(data_out_b_B1),
+        .oversample_offset(oversample_offset_B1),
+        .busy(busy_all[2]),
+        .done(done_all[2])
+    );
+
+    rhd_spi_master B2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_B),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_B2),
+        .b_data_out(data_out_b_B2),
+        .oversample_offset(oversample_offset_B2),
+        .busy(busy_all[3]),
+        .done(done_all[3])
+    );
+
+    rhd_spi_master C1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_C),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_C1),
+        .b_data_out(data_out_b_C1),
+        .oversample_offset(oversample_offset_C1),
+        .busy(busy_all[4]),
+        .done(done_all[4])
+    );
+
+    rhd_spi_master C2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_C),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_C2),
+        .b_data_out(data_out_b_C2),
+        .oversample_offset(oversample_offset_C2),
+        .busy(busy_all[5]),
+        .done(done_all[5])
+    );
+
+    rhd_spi_master D1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_D),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_D1),
+        .b_data_out(data_out_b_D1),
+        .oversample_offset(oversample_offset_D1),
+        .busy(busy_all[6]),
+        .done(done_all[6])
+    );
+
+    rhd_spi_master D2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_D),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_D2),
+        .b_data_out(data_out_b_D2),
+        .oversample_offset(oversample_offset_D2),
+        .busy(busy_all[7]),
+        .done(done_all[7])
+    );
+
+    rhd_spi_master E1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_E),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_E1),
+        .b_data_out(data_out_b_E1),
+        .oversample_offset(oversample_offset_E1),
+        .busy(busy_all[8]),
+        .done(done_all[8])
+    );
+
+    rhd_spi_master E2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_E),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_E2),
+        .b_data_out(data_out_b_E2),
+        .oversample_offset(oversample_offset_E2),
+        .busy(busy_all[9]),
+        .done(done_all[9])
+    );
+
+    rhd_spi_master F1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_F),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_F1),
+        .b_data_out(data_out_b_F1),
+        .oversample_offset(oversample_offset_F1),
+        .busy(busy_all[10]),
+        .done(done_all[10])
+    );
+
+    rhd_spi_master F2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_F),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_F2),
+        .b_data_out(data_out_b_F2),
+        .oversample_offset(oversample_offset_F2),
+        .busy(busy_all[11]),
+        .done(done_all[11])
+    );
+
+    rhd_spi_master G1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_G),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_G1),
+        .b_data_out(data_out_b_G1),
+        .oversample_offset(oversample_offset_G1),
+        .busy(busy_all[12]),
+        .done(done_all[12])
+    );
+
+    rhd_spi_master G2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_G),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_G2),
+        .b_data_out(data_out_b_G2),
+        .oversample_offset(oversample_offset_G2),
+        .busy(busy_all[13]),
+        .done(done_all[13])
+    );
+
+    rhd_spi_master H1(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_H),
+        .CS(CS),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_H1),
+        .b_data_out(data_out_b_H1),
+        .oversample_offset(oversample_offset_H1),
+        .busy(busy_all[14]),
+        .done(done_all[14])
+    );
+
+    rhd_spi_master H2(
+        .clk(clk),
+        .rstn(rstn),
+        .SCLK(),
+        .MOSI(),
+        .MISO(MISO2_H),
+        .CS(),
+        .start(start),
+        .data_in(data_in),
+        .a_data_out(data_out_a_H2),
+        .b_data_out(data_out_b_H2),
+        .oversample_offset(oversample_offset_H2),
+        .busy(busy_all[15]),
+        .done(done_all[15])
     );
 
 
@@ -224,8 +523,86 @@ module rhd_2048 (
                     start = 0;
                     if (&done_all) begin
                         if (channel < CHANNELS_PER_ADC + SPI_CONVERT_DELAY && channel >= SPI_CONVERT_DELAY) begin
-                            data_out[(data_gather_index * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_A1;
-                            data_out[((CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_A1;
+                            data_out[((0 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_A1;
+                            data_out[((1 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_A1;
+                            data_out[((2 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_A2;
+                            data_out[((3 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_A2;
+                            
+                            data_out[((4 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_B1;
+                            data_out[((5 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_B1;
+                            data_out[((6 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_B2;
+                            data_out[((7 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_B2;
+
+                            data_out[((8 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_C1;
+                            data_out[((9 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_C1;
+                            data_out[((10 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_C2;
+                            data_out[((11 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_C2;
+
+                            data_out[((12 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_D1;
+                            data_out[((13 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_D1;
+                            data_out[((14 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_D2;
+                            data_out[((15 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_D2;
+
+                            data_out[((16 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_E1;
+                            data_out[((17 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_E1;
+                            data_out[((18 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_E2;
+                            data_out[((19 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_E2;
+                            
+                            data_out[((20 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_F1;
+                            data_out[((21 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_F1;
+                            data_out[((22 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_F2;
+                            data_out[((23 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_F2;
+
+                            data_out[((24 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_G1;
+                            data_out[((25 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_G1;
+                            data_out[((26 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_G2;
+                            data_out[((27 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_G2;
+
+                            data_out[((28 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_H1;
+                            data_out[((29 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_H1;
+                            data_out[((30 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_H2;
+                            data_out[((31 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_H2;
+
+                            data_out[((32 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_I1;
+                            data_out[((33 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_I1;
+                            data_out[((34 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_I2;
+                            data_out[((35 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_I2;
+                            
+                            data_out[((36 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_J1;
+                            data_out[((37 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_J1;
+                            data_out[((38 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_J2;
+                            data_out[((39 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_J2;
+
+                            data_out[((40 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_K1;
+                            data_out[((41 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_K1;
+                            data_out[((42 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_K2;
+                            data_out[((43 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_K2;
+
+                            data_out[((44 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_L1;
+                            data_out[((45 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_L1;
+                            data_out[((46 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_L2;
+                            data_out[((47 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_L2;
+
+                            data_out[((48 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_M1;
+                            data_out[((49 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_M1;
+                            data_out[((50 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_M2;
+                            data_out[((51 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_M2;
+                            
+                            data_out[((52 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_N1;
+                            data_out[((53 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_N1;
+                            data_out[((54 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_N2;
+                            data_out[((55 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_N2;
+
+                            data_out[((56 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_O1;
+                            data_out[((57 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_O1;
+                            data_out[((58 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_O2;
+                            data_out[((59 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_O2;
+
+                            data_out[((60 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_P1;
+                            data_out[((61 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_P1;
+                            data_out[((62 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_a_P2;
+                            data_out[((63 * CHANNELS_PER_ADC + data_gather_index) * ADC_SAMPLE_BIT_RESOLUTION) +: ADC_SAMPLE_BIT_RESOLUTION] <= data_out_b_P2;
+                            
                         end
                         channel = channel + 1;
                         if (channel == DEFAULT_CHANNELS)
--- src/tb_rhd_2048.v
+++ src/tb_rhd_2048.v
@@ -60,8 +60,73 @@ module tb_rhd_2048 ();
         .CS(CS),
         .MOSI(MOSI),
         .SCLK(SCLK),
-        .MISO1_A(MISO1_A)
+        .MISO1_A(MISO1_A),
+        .MISO2_A(MISO2_A),
+        .MISO1_B(MISO1_B),
+        .MISO2_B(MISO2_B),
+        .MISO1_C(MISO1_C),
+        .MISO2_C(MISO2_C),
+        .MISO1_D(MISO1_D),
+        .MISO2_D(MISO2_D),
+        .MISO1_E(MISO1_E),
+        .MISO2_E(MISO2_E),
+        .MISO1_F(MISO1_F),
+        .MISO2_F(MISO2_F),
+        .MISO1_G(MISO1_G),
+        .MISO2_G(MISO2_G),
+        .MISO1_H(MISO1_H),
+        .MISO2_H(MISO2_H),
+        .MISO1_I(MISO1_I),
+        .MISO2_I(MISO2_I),
+        .MISO1_J(MISO1_J),
+        .MISO2_J(MISO2_J),
+        .MISO1_K(MISO1_K),
+        .MISO2_K(MISO2_K),
+        .MISO1_L(MISO1_L),
+        .MISO2_L(MISO2_L),
+        .MISO1_M(MISO1_M),
+        .MISO2_M(MISO2_M),
+        .MISO1_N(MISO1_N),
+        .MISO2_N(MISO2_N),
+        .MISO1_O(MISO1_O),
+        .MISO2_O(MISO2_O),
+        .MISO1_P(MISO1_P),
+        .MISO2_P(MISO2_P)
     );
+
+    wire MISO1_A;
+    wire MISO2_A;
+    wire MISO1_B;
+    wire MISO2_B;
+    wire MISO1_C;
+    wire MISO2_C;
+    wire MISO1_D;
+    wire MISO2_D;
+    wire MISO1_E;
+    wire MISO2_E;
+    wire MISO1_F;
+    wire MISO2_F;
+    wire MISO1_G;
+    wire MISO2_G;
+    wire MISO1_H;
+    wire MISO2_H;
+    wire MISO1_I;
+    wire MISO2_I;
+    wire MISO1_J;
+    wire MISO2_J;
+    wire MISO1_K;
+    wire MISO2_K;
+    wire MISO1_L;
+    wire MISO2_L;
+    wire MISO1_M;
+    wire MISO2_M;
+    wire MISO1_N;
+    wire MISO2_N;
+    wire MISO1_O;
+    wire MISO2_O;
+    wire MISO1_P;
+    wire MISO2_P;
+
 
     rhd_spi_slave #(.STARTING_SEED(0)) A1_slave(
         .SCLK(SCLK),
@@ -77,6 +142,306 @@ module tb_rhd_2048 ();
         .SCLK(SCLK),
         .MOSI(MOSI),
         .MISO(MISO2_A),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(144)) B1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_B),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(208)) B2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_B),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(288)) C1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_C),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(352)) C2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_C),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(432)) D1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_D),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(496)) D2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_D),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(576)) E1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_E),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(640)) E2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_E),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(720)) F1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_F),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(784)) F2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_F),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(864)) G1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_G),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(928)) G2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_G),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1008)) H1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_H),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1072)) H2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_H),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1152)) I1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_I),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1216)) I2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_I),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1296)) J1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_J),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1360)) J2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_J),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1440)) K1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_K),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1504)) K2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_K),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1584)) L1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_L),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1648)) L2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_L),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1728)) M1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_M),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1792)) M2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_M),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1872)) N1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_N),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(1936)) N2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_N),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(2016)) O1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_O),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(2080)) O2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_O),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(2160)) P1_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO1_P),
+        .CS(CS),
+        .channel(rhd_channel),
+        .rstn(rstn),
+        .clk(clk)
+    );
+
+    rhd_spi_slave #(.STARTING_SEED(2224)) P2_slave(
+        .SCLK(SCLK),
+        .MOSI(MOSI),
+        .MISO(MISO2_P),
         .CS(CS),
         .channel(rhd_channel),
         .rstn(rstn),
