0f3ccd8d52b4fa3b597b6325ce84fe69f6287555
Hao Le <anhhao135@gmail.com>
2024-10-03 10:14:27 -0700

    Update seeg.v

--- src/seeg.v
+++ src/seeg.v
@@ -239,15 +239,15 @@ module seeg (
 
     assign clk_rhs = zcheck_in_progress ? clk_rhs_zcheck : clk;
 
-    /*
+    
     localparam RHD_CHANNELS = 2048;
     localparam RHS_CHANNELS = 256;
-    */
-
+    
+    /*
     
     localparam RHD_CHANNELS = 1;
     localparam RHS_CHANNELS = 1;
-    
+    */
 
     localparam RHD_64_BIT_CHUNKS = 512; //for recording
     localparam RHS_64_BIT_CHUNKS = 64; //for recording
