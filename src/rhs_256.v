996abea782176dd01caee7e48847473bad19953f
Hao Le <51278143+anhhao135@users.noreply.github.com>
2025-04-07 10:20:12 -0700

    Update rhs_256.v

--- src/rhs_256.v
+++ src/rhs_256.v
@@ -1574,8 +1574,9 @@ module rhs_256 (
                             data_out_G_reg <= data_out_G[31:16];
                             data_out_H_reg <= data_out_H[31:16];
                             //data_out_I_reg <= data_out_I[31:16];
-                            data_out_I_reg <= stimulation_magnitude_debug; //channel I 
-                            data_out_J_reg <= data_out_J[31:16];
+                            data_out_I_reg <= stimulation_magnitude_debug; //channel I shows stim waveform
+                            //data_out_J_reg <= data_out_J[31:16];
+                            data_out_J_reg <= busy_stim;
                             data_out_K_reg <= data_out_K[31:16];
                             data_out_L_reg <= data_out_L[31:16];
                             data_out_M_reg <= data_out_M[31:16];
